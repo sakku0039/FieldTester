fc10dadd81f3a7ed0287ad8baec2775432beafa4
裕章 櫻井 <sakuraih@liberty-kobe.co.jp>
2026-06-24 16:36:22 +0900

    docs: add initial design documentation for FieldTester application

--- docs/design/reference-code/SettingsResolution/ISettingResolver.cs
+++
@@ -1,9 +0,0 @@
-namespace FieldTester.SyncAgent.SettingsResolution;
-
-public interface ISettingResolver
-{
-    string Provider { get; }
-    Task<ResolvedLocalSystemSettings> ResolveAsync(
-        string localSettingFilePath,
-        CancellationToken cancellationToken);
-}
